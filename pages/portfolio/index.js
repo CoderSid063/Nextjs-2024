d33dc24e57935c58c5214326056c9e0ddb6a77a8
CoderSid063 <sudhansupanda063@gmail.com>
2024-05-22 16:38:22 +0530

    setup page route and dynamic routes

---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+function Portfolio() {
+  return (
+    <div>
+      <h1>this is portfolio page</h1>
+    </div>
+  );
+}
+
+export default Portfolio;
---
+++ pages/portfolio/list.js
@@ -0,0 +1,7 @@
+export default function ListPage(params) {
+  return (
+    <div>
+      <h1>This is List page</h1>
+    </div>
+  );
+}
