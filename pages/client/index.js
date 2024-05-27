f521da7ec8ca2a969c6b9569ffb45533a136c368
CoderSid063 <sudhansupanda063@gmail.com>
2024-05-27 17:24:33 +0530

    feat : Navigate Dynamically using Link

--- pages/client/index.js
+++ pages/client/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function ClientPage() {
+  const clients = [
+    { id: "max", name: "maximus" },
+    { id: "sam", name: "samson" },
+    { id: "vicky", name: "vicky" },
+    { id: "chicku", name: "virat" },
+  ];
   return (
     <div>
       <h1>The Client Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`client/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
 function HomePage() {
   return (
     <div>
       <h1>This is Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/client">Client</Link>
+        </li>
+      </ul>
     </div>
   );
 }
